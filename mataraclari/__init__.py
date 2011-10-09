b80344171994df79607f2fe245040ef5169ef013
Yaşar Arabacı <yasar11732@gmail.com>
2011-10-10 01:13:22 +0300

    None codebase related improvements
    * Added license notice
    * Improved information in setup.py
    * Created archLinux PKGBUILD file
    * Created archLinux i686 binary package.

--- mataraclari/__init__.py
+++ mataraclari/__init__.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+    This Python module is part of the mataraclari package, which provides
+    some mathematical shortcuts for Python.
+
+    
+    Copyright (C) 2011  Yaşar Arabacı
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
 from _mathGerecleri import *
 from _carpanlar import *
 
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from distutils.core import setup, Extension
 import os
 
@@ -6,10 +7,30 @@ pack_dir = os.path.join(os.path.dirname(__file__),"mataraclari")
 mathgerecleri = Extension('mataraclari._mathGerecleri',
                     sources = ['mataraclari/mathgerecler.c'])
 
-setup (name="mataraclari",
-       version = '0.1',
-       description = 'Provides some math utilities',
-       packages = ["mataraclari"],
-       package_dir = {"mataraclari":pack_dir},
-       ext_modules = [mathgerecleri]
-       )
+long_description = """ This package provides couple of functions
+for solving some mathematical problems, like finding primes. They are
+intended to be very fast, and they are as fast as they can be. Hopefully,
+this will spare you from reinventing the whell when you need to do some
+basic mathematical calculations. Currently readme file is in Turkish, and
+yet no other documentation is available, but this may change in future."""
+
+setup(
+    name="mataraclari",
+    version = '0.12',
+    description = 'Provides some math utilities',
+    author = 'Yaşar Arabacı',
+    author_email = 'yasar11732@gmail.com',
+    long_description = long_description,
+    license = "GPL v3",
+    url = 'http://yasar.serveblog.net',
+    packages = ["mataraclari"],
+    package_dir = {"mataraclari":pack_dir},
+    ext_modules = [mathgerecleri],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        ]
+    )
